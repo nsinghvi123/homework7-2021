075c3541f83367f9c18f9cc64a499e902181167a
Natasha Singhvi <60042921+nsinghvi123@users.noreply.github.com>
2021-04-01 22:11:48 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,71 @@
+var video;
+
+window.addEventListener("load", function() {
+	video = document.querySelector("#player1");
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.95;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.95;
+	console.log(video.playbackRate);
+});
+
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 15 < video.duration){
+		video.currentTime += 15;
+		console.log(video.currentTime);
+	} else {
+		video.currentTime = 0;
+		console.log(video.currentTime);
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == true){
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+	else {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "UnMute";
+	}
+});
+
+
+document.querySelector("#slider").addEventListener("change", function() {
+	console.log(this.value);
+	var slider = this.value;
+	video.volume = slider / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Vintage");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Back to normal");
+	video.classList.remove("oldSchool");
+});
+
+
